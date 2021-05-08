5c3ffe66384bdb9df8eaf9beaea93abaad5b9d5f
Arsalan-Aslam <arsalanaslam89@gmail.com>
2021-05-08 09:50:22 +0500

    Project 2 JS
    Added project 2 JS

--- project-2/script.js
+++ project-2/script.js
@@ -5,6 +5,26 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = movieSelect.value;
+let ticketPrice = +movieSelect.value;
 
-console.log(ticketPrice);
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount
+    total.innerText = "$" + selectedSeatsCount * ticketPrice
+    console.log(selectedSeatsCount);
+}
+
+// 1. Event listener for container to check for click on seats
+container.addEventListener('click', e => {
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
+    }
+})
+
+// 2. Event listener for movie select
+movieSelect.addEventListener('change', e => {
+    ticketPrice = e.target.value;
+    updateSelectedCount();
+})
