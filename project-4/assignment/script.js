b8b5af6e8ec1555819b7f4ff9d31911f2d2cdc48
Arsalan-Aslam <arsalanaslam89@gmail.com>
2021-05-29 11:34:34 +0500

    update

--- project-4/assignment/script.js
+++ project-4/assignment/script.js
@@ -3,6 +3,7 @@ const baseCurrency = document.getElementById('base-currency');
 const baseAmount = document.getElementById('base-amount');
 const exchangeRates = document.getElementById('exchange-rates');
 const paste = document.getElementById('paste');
+const ratesTable = document.getElementById("exchange-rates");
 
 // Fetch exchange rates & update the DOM
 function calculate() {
@@ -13,19 +14,28 @@ function calculate() {
     fetch(`https://v6.exchangerate-api.com/v6/8acf3f578e33647a1a9ee130/latest/${baseCurrencyCode}`)
         .then(res => res.json())
         .then(data => {
-            const conversionRates = data.conversion_rates;
-
-            const keys = Object.keys(conversionRates);
+            const rates = data.conversion_rates;
+            const keys = Object.keys(rates);
             
-            
-        
+            keys.forEach((key) => {
+                const table = document.getElementById('rates-table');
+                const row = `<tr>
+                                <td>${key}</td>
+                                <td>${rates[key]}</td>
+                            </tr>`;
+                console.log(row);
+                table.innerHTML += row;
+                
             });
-        };
-
-
+        });
+                
+};
 //Event Listener
 
-baseCurrency.addEventListener('change', calculate);
+baseCurrency.addEventListener("change", calculate);
 
-baseAmount.addEventListener('input', calculate);
+baseAmount.addEventListener("input", calculate);
 
+
+// Execute calculate function on page load
+calculate();
