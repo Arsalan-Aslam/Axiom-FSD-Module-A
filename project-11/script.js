734e0c100b30a4f36023ba063d9c2a7244c8e03b
Arsalan-Aslam <arsalanaslam89@gmail.com>
2021-07-18 20:08:34 +0500

    Updated JS

--- project-11/script.js
+++ project-11/script.js
@@ -1,22 +1,23 @@
 // Get the DOM elements
 
-const filter = document.getElementById('filter-container');
+const filter = document.getElementById('filter');
 const newsFeed = document.getElementById('news-feed-container');
 const loader = document.getElementById('loader');
 
-// Global variable for number of posts tp fetch api call and current page
+// Global variable for number of posts to fetch API call and current page
 let limit = 5;
 let page = 1;
 
 // Function to asynchronously fetch posts from API
 async function fetchPosts() {
     // Fetch posts from the JSON Placeholder API
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?limit=${limit}&page=${page}`);
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`);
     const data = await res.json();
     return data;
+    console.log(data);
 };
 
-// Function to render the posts fetch from API
+// Function to render the posts fetched from API
 async function renderPosts() {
     // Fetch the data from the API that we want to render
     const posts = await fetchPosts();
@@ -42,19 +43,51 @@ async function renderPosts() {
 function showLoader() {
     // Display the CSS loader animation
     loader.classList.add('show');
+    // Increment the page global variable by 1
+    page++;
+    // Render the posts from the new page
+    renderPosts();
+    // Remove the loader
+    loader.classList.remove('show');
 }
+
+// Function to filter posts
+function filterPosts(e) {
+    const filterKeyword = e.target.value.toLowerCase();
+    // Get all post data from DOM
+    const posts = document.querySelectorAll('.post');
+    // Process all posts in the posts node list
+    posts.forEach( post => {
+        // Get the title text
+        const title = post.querySelector('.post-title').innerText;
+        // Get the body text
+        const body = post.querySelector('.post-body').innerText;
+        // Check if filterKeyword exists in title or body
+        if( title.indexOf(filterKeyword) >=0 || body.indexOf(filterKeyword) >=0) {
+            // Display the post if the filterKeyword exists in title or body of post
+            post.style.display = 'flex';
+        } else {
+            // Hide the post if the filterKeyword does not exist in title or body of post
+            post.style.display = 'none';
+        }
+    });
+
+};
+
 
 // Event Listener
 // Listen for scroll in the browser window
 window.addEventListener('scroll', () => {
     // Destructuring properties from DOM
-    const { ScrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
     // Check if scrolled to the bottom of page
-    if ( scrollTop + clientHeight >= scrollHeight - 50 ) {
+    if ( scrollTop + clientHeight >= scrollHeight - 1 ) {
         // Display the loader animation
         showLoader(); 
     }
-
-    console.log(document.documentElement);
 });
+
+// Listen for input in the filter input
+filter.addEventListener('input', filterPosts);
+
 renderPosts();
