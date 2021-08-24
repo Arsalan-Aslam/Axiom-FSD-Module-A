4b45cfb84b013cd8b065d90bc307b038a4276df0
Arsalan-Aslam <arsalanaslam89@gmail.com>
2021-08-24 08:02:43 +0500

    Update script.js

--- project-13/script.js
+++ project-13/script.js
@@ -10,3 +10,26 @@ const questionInput = document.getElementById('question');
 const answerInput = document.getElementById('answer');
 const addCardSubmitBtn = document.getElementById('add-card-btn');
 const addCardContainer = document.getElementById('add-card-container');
+
+// ID of current card
+
+let currentCardID = 0;
+
+// Collection of cards DOM Elements
+const cards = [];
+
+// Collection of Cards data
+const cardData = [
+    {
+        question: 'What is React?',
+        answer: 'React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.'
+    },
+    {
+        question: 'What is HTML',
+        answer: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.'
+    },
+    {
+        question: 'What is MongoDB',
+        answer: 'MongoDB is a source-available cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with optional schemas. MongoDB is developed by MongoDB Inc. and licensed under the Server Side Public License.'
+    }
+];
