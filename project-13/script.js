c4a7cc8091982a48c662202da3f1514b03f4b498
Arsalan-Aslam <arsalanaslam89@gmail.com>
2021-08-27 18:41:56 +0500

    updated JS and CSS

--- project-13/script.js
+++ project-13/script.js
@@ -12,7 +12,6 @@ const addCardSubmitBtn = document.getElementById('add-card-btn');
 const addCardContainer = document.getElementById('add-card-container');
 
 // ID of current card
-
 let currentCardID = 0;
 
 // Collection of cards DOM Elements
@@ -21,15 +20,64 @@ const cards = [];
 // Collection of Cards data
 const cardData = [
     {
+        // 0
         question: 'What is React?',
         answer: 'React is a free and open-source front-end JavaScript library for building user interfaces or UI components. It is maintained by Facebook and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications.'
     },
     {
+        // 1
         question: 'What is HTML',
         answer: 'The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.'
     },
     {
+        // 2
         question: 'What is MongoDB',
         answer: 'MongoDB is a source-available cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with optional schemas. MongoDB is developed by MongoDB Inc. and licensed under the Server Side Public License.'
     }
 ];
+
+// Function to update the currentCardNave
+function currentCardNav () {
+    currentCardNav.innerText = `${currentCardID + 1} / ${cards.length}`;
+}
+
+// Function to generate cards based on cardData
+function generateCards () {
+    // Itterate over cardData and generate cards
+    cardData.forEach( (data, index) => generateCard( data, index) );
+};
+
+// function to generate a single card
+function generateCard (data, index) {
+    // create a div element for the card
+    const card = document.createElement('div');
+    // assign the card class
+    card.classList.add('card');
+    // Only make the first card active
+    if (index === 0) {
+        // If it is the first card, assign active class
+        card.classList.add('active');
+    }
+    // Create the card content structure
+    card.innerHTML = `
+        <div class="inside-card">
+            <div class="card-front">
+                <p>${data.question}</p>
+            </div>
+            <div class="card-back">
+                <p>${data.answer}</p>
+            </div>
+        </div>    
+    `;
+    // Listen for click on card
+    card.addEventListener('click', () => card.classList.toggle('show-answer'));
+    // Add the card into the DOM
+    cards.push(card);
+    // append the card into the cardsContainer
+    cardsContainer.appendChild(card);
+    // Update text for currentCardNav
+    updateCurrentCardNav();
+
+};
+
+generateCards();
